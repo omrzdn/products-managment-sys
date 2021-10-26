1a36e7b3198d9b8299abf69fef96743d46919f84
Omar Zidan <35118764+omrzdn@users.noreply.github.com>
2021-10-26 21:17:24 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $connection=new PDO('mysql:host=localhost;dbname=vendor;','root','');
+    $connection=new PDO('mysql:host=ec2-52-5-247-46.compute-1.amazonaws.com
+;dbname=d72102pnmqif5r;','xfcktdsnifoglw','7aa63198b623a2a7f940b8f310f5c303789b8947bc13e36e94632902f280de99');
 }catch (PDOException $exception){
     echo $exception->getMessage();
 }
